56293dde90762265f41ac16b7ac04b6405d9dd65
Rodrigo Faria <rde-fari@c2r6s4.42porto.com>
2024-05-20 18:13:01 +0100

    get_next_line template

---
+++ get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rde-fari <rde-fari@student.42poto.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/17 17:24:03 by rde-fari          #+#    #+#             */
+/*   Updated: 2024/05/17 19:08:20 by rde-fari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+//---------Library----------//
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+//---------Get Next Line Functions---------//
+
+
+
+//---------End If---------//
+#endif
