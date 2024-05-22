6a4c6387f1e0cf475b640d06bbfb0ec3a19bfdbf
Rodrigo Faria <rde-fari@c2r4s6.42porto.com>
2024-05-22 19:37:27 +0100

    get_next_line Update

--- get_next_line.c
+++ get_next_line.c
@@ -6,54 +6,129 @@
 /*   By: rde-fari <rde-fari@student.42poto.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 17:22:48 by rde-fari          #+#    #+#             */
-/*   Updated: 2024/05/21 19:05:15 by rde-fari         ###   ########.fr       */
+/*   Updated: 2024/05/22 19:35:23 by rde-fari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/* 
-*	GET_NEXT_LINE
-*	-------------
-*	DESCRIPTION
-*	This function takes an opened file descriptor and returns its next line.
-*	This function has undefined behavior when reading from a binary file.
-*	PARAMETERS
-*	#1. A file descriptor 
-*	RETURN VALUES
-*	If successful, get_next_line returns a string with the full line ending in
-*	a line break (`\n`) when there is one. 
-*	If an error occurs, or there's nothing more to read, it returns NULL.
-*	----------------------------------------------------------------------------
-*/
-
 #include "get_next_line.h"
 
-// int fd sera sempre sempre a primeira linha de cda arquivo. Semnpre que o programa e executado ele pega a proxima.
-//size_t read(int fildes, void *buf, size_t nbyte);
-char *get_next_line(int fd)// fd == fd1 (text1.txt).
-{
-	char	*temp;
-	int		buf;
-	int		i;
+int	ft_strchr(const char *s, int c);
 
-	i = 0;
-	while (i != BUFFER_SIZE)
-	{
-		temp = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-		if (!temp)
-			return (NULL);
-		buf = read(fd, temp, 5);
-		printf("->%s\n", temp);
-		free(temp);
-		i++;
-	}
-	return (temp);
+char	*get_next_line(int fd)
+{
+/* 	static char	*storage; */
+	const char	*storage;
+	char		*line;
+	int			read_return;
+
+	line = (char *)malloc(sizeof(char) * BUFFER_SIZE);
+	if (!line)
+		return (NULL);
+	read_return = read(fd, line, BUFFER_SIZE);
+	while (ft_strchr(line, '\0') != 0)
+		storage = ft_strjoin(storage, line);
+	printf("Resultado atual: %s\n", storage);
+	return (line);
 }
 
+int	ft_strchr(const char *s, int c)
+{
+	while ((*s != '\0') && (*s != c))
+	{
+		s++;
+	}
+	if (*s == c)
+	{
+		return (1);
+	}
+	return (0);
+}
 
+/* char	*ft_strdup(const char *s)
+{
+	int		i;
+	int		size;
+	char	*dup;
+
+	size = ft_strlen(s);
+	dup = malloc((sizeof(char) * size) + 1);
+	if (!dup)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		dup[i] = s[i];
+		i++;
+	}
+	if (i == size)
+		dup[i] = '\0';
+	return (dup);
+} */
+
+/* size_t	ft_strlen(const char *str)
+{
+	size_t		i;
+	const char	*s;
+
+	s = str;
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+} */
+
+/* char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	char	*substr;
+
+	if (!s || start >= ft_strlen(s) || len == 0)
+		return (ft_strdup(""));
+	if (ft_strlen(s + start) < len)
+		len = ft_strlen(s + start);
+	substr = malloc(sizeof(char) * (len + 1));
+	if (!substr)
+		return (NULL);
+	i = 0;
+	while (i < len && s[start + i])
+	{
+		substr[i] = s[start + i];
+		i++;
+	}
+	return (substr);
+} */
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char			*strjoin;
+	size_t			malloc_size;
+	size_t			i;
+	size_t			j;
+
+	malloc_size = ((ft_strlen(s1) + ft_strlen(s2)) + 1);
+	strjoin = malloc(sizeof(char) * malloc_size);
+	if (!strjoin)
+		return (NULL);
+	i = 0;
+	while (i < (ft_strlen((const char *)s1)))
+	{
+		strjoin[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (i < (malloc_size - 1))
+	{
+		strjoin[i] = s2[j];
+		i++;
+		j++;
+	}
+	strjoin[i] = '\0';
+	return (strjoin);
+}
 
 int main(void)
 {
-/* 	char	*line;
+/*  	char	*line;
 	int		i;
 	int		fd1;
 	int		fd2;
--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,13 @@
 /*   By: rde-fari <rde-fari@student.42poto.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 17:24:03 by rde-fari          #+#    #+#             */
-/*   Updated: 2024/05/21 18:14:06 by rde-fari         ###   ########.fr       */
+/*   Updated: 2024/05/22 19:04:54 by rde-fari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 10
 # endif
@@ -25,11 +26,7 @@
 
 //---------Get Next Line Functions---------//
 
-int		ft_strlen(char *str);
-
 char	*get_next_line(int fd);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strjoin(char const *s1, char const *s2);
 
 //---------End If---------//
 #endif
