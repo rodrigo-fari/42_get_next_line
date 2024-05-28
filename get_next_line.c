aac7e71cbbf18c79105a78c1444ea9d669e83bf9
Rodrigo Faria <rde-fari@c1r11s2.42porto.com>
2024-05-28 15:26:31 +0100

    Finished get_next_line

--- get_next_line.c
+++ get_next_line.c
@@ -6,157 +6,95 @@
 /*   By: rde-fari <rde-fari@student.42poto.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 17:22:48 by rde-fari          #+#    #+#             */
-/*   Updated: 2024/05/22 19:35:23 by rde-fari         ###   ########.fr       */
+/*   Updated: 2024/05/28 15:21:38 by rde-fari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	ft_strchr(const char *s, int c);
-
 char	*get_next_line(int fd)
 {
-/* 	static char	*storage; */
-	const char	*storage;
+	static char	*ptr;
 	char		*line;
-	int			read_return;
 
-	line = (char *)malloc(sizeof(char) * BUFFER_SIZE);
-	if (!line)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	read_return = read(fd, line, BUFFER_SIZE);
-	while (ft_strchr(line, '\0') != 0)
-		storage = ft_strjoin(storage, line);
-	printf("Resultado atual: %s\n", storage);
+	ptr = file_reader(ptr, fd);
+	if (!ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	line = get_line(ptr);
+	ptr = remaining(ptr);
 	return (line);
 }
 
-int	ft_strchr(const char *s, int c)
+char	*file_reader(char *ptr, int fd)
 {
-	while ((*s != '\0') && (*s != c))
+	char	*temp_ptr;
+	int		read_return;
+
+	temp_ptr = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	if (!temp_ptr)
+		return (NULL);
+	read_return = 1;
+	while (!nline_scan(ptr, '\n') && read_return > 0)
 	{
-		s++;
+		read_return = read(fd, temp_ptr, BUFFER_SIZE);
+		if (read_return < 0)
+		{
+			if (ptr)
+				free(ptr);
+			free(temp_ptr);
+			return (NULL);
+		}
+		temp_ptr[read_return] = '\0';
+		ptr = ft_strjoin(ptr, temp_ptr);
 	}
-	if (*s == c)
-	{
-		return (1);
-	}
-	return (0);
+	free(temp_ptr);
+	return (ptr);
 }
 
-/* char	*ft_strdup(const char *s)
+char	*get_line(char	*ptr)
 {
+	char	*str;
 	int		i;
-	int		size;
-	char	*dup;
+	int		j;
 
-	size = ft_strlen(s);
-	dup = malloc((sizeof(char) * size) + 1);
-	if (!dup)
+	if (ptr[0] == '\0')
 		return (NULL);
 	i = 0;
-	while (s[i])
-	{
-		dup[i] = s[i];
+	while (ptr[i] && ptr[i] != '\n')
 		i++;
-	}
-	if (i == size)
-		dup[i] = '\0';
-	return (dup);
-} */
-
-/* size_t	ft_strlen(const char *str)
-{
-	size_t		i;
-	const char	*s;
-
-	s = str;
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
-} */
-
-/* char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	size_t	i;
-	char	*substr;
-
-	if (!s || start >= ft_strlen(s) || len == 0)
-		return (ft_strdup(""));
-	if (ft_strlen(s + start) < len)
-		len = ft_strlen(s + start);
-	substr = malloc(sizeof(char) * (len + 1));
-	if (!substr)
-		return (NULL);
-	i = 0;
-	while (i < len && s[start + i])
-	{
-		substr[i] = s[start + i];
-		i++;
-	}
-	return (substr);
-} */
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	char			*strjoin;
-	size_t			malloc_size;
-	size_t			i;
-	size_t			j;
-
-	malloc_size = ((ft_strlen(s1) + ft_strlen(s2)) + 1);
-	strjoin = malloc(sizeof(char) * malloc_size);
-	if (!strjoin)
-		return (NULL);
-	i = 0;
-	while (i < (ft_strlen((const char *)s1)))
-	{
-		strjoin[i] = s1[i];
-		i++;
-	}
-	j = 0;
-	while (i < (malloc_size - 1))
-	{
-		strjoin[i] = s2[j];
-		i++;
+	j = i;
+	if (ptr[i] == '\n')
 		j++;
-	}
-	strjoin[i] = '\0';
-	return (strjoin);
+	str = ft_calloc(j + 1, sizeof(char));
+	if (!str)
+		return (NULL);
+	ft_strcpy(ptr, str, 1);
+	return (str);
 }
 
-int main(void)
+char	*remaining(char *ptr)
 {
-/*  	char	*line;
+	char	*remaining;
 	int		i;
-	int		fd1;
-	int		fd2;
-	int		fd3;
 
-
-	fd1 = open("text1.txt", O_RDONLY);
-	fd2 = open("text2.txt", O_RDONLY);
-	fd3 = open("text3.txt", O_RDONLY);
-	
-	i = 1;
-	while (i < 7)
-	{
-		line = get_next_line(fd1);
-		printf("line [%02d]: %s", i, line);
-		free(line);
-		line = get_next_line(fd2);
-		printf("line [%02d]: %s", i, line);
-		free(line);
-		line = get_next_line(fd3);
-		printf("line [%02d]: %s", i, line);
-		free(line);
+	i = 0;
+	while (ptr[i] && ptr[i] != '\n')
 		i++;
+	if (ptr[i] == '\0')
+	{
+		free(ptr);
+		return (NULL);
 	}
-	close(fd1);
-	close(fd2);
-	close(fd3); */
-	int fd1 = open("text1.txt", O_RDONLY );
-	get_next_line(fd1);
-	return (0);
+	i++;
+	remaining = ft_calloc(ft_strlen(ptr + i) + 1, sizeof(char));
+	if (!remaining)
+		return (NULL);
+	ft_strcpy(ptr + i, remaining, 0);
+	free(ptr);
+	return (remaining);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,14 @@
 /*   By: rde-fari <rde-fari@student.42poto.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 17:24:03 by rde-fari          #+#    #+#             */
-/*   Updated: 2024/05/22 19:04:54 by rde-fari         ###   ########.fr       */
+/*   Updated: 2024/05/28 15:19:32 by rde-fari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+//---------BUFFER_SIZE----------//
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 10
@@ -26,7 +28,22 @@
 
 //---------Get Next Line Functions---------//
 
+char	*get_line(char	*ptr);
+char	*remaining(char *ptr);
 char	*get_next_line(int fd);
+char	*file_reader(char *ptr, int fd);
+
+//---------Get Next Line Utils Functions---------//
+
+int		nline_scan(char *str, int c);
+
+size_t	ft_strlen(char *str);
+
+void	*ft_calloc(size_t nmemb, size_t size);
+
+char	*ft_strjoin(char *str1, char *str2);
+char	*ft_strcpy(char *src, char *dest, int param);
 
 //---------End If---------//
+
 #endif
